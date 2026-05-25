cc4e2e75a38559270bacfa4a4098b71c5acbc485
Mobolaji Richard <104364699+MobolajiRichard@users.noreply.github.com>
2026-05-25 08:34:20 +0100

    feat: removed unused backend folder

--- backend/src/auth/dto/reset-password.dto.ts
+++
@@ -1,6 +0,0 @@
-import { IsString, MinLength } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
-
-export class ResetPasswordDto {
-  @ApiProperty() @IsString() @MinLength(8) password: string;
-}
--- backend/src/users/dto/update-profile.dto.ts
+++
@@ -1,6 +0,0 @@
-import { IsOptional, IsString } from 'class-validator';
-import { ApiPropertyOptional } from '@nestjs/swagger';
-
-export class UpdateProfileDto {
-  @ApiPropertyOptional() @IsOptional() @IsString() name?: string;
-}
